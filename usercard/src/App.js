f43c89288d33a7dd745c9b927789414620155c7c
Kenneth Morlock <kennethmorlock@Kenneths-MBP.fios-router.home>
2020-04-07 16:24:40 -0400

    api added and usercard created

--- usercard/src/App.js
+++ usercard/src/App.js
@@ -1,26 +1,37 @@
 import React from 'react';
 import logo from './logo.svg';
-import './App.css';
+import './App.css'; import React from 'react';
+import axios from 'axios';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.component {
+  constructor() {
+    super ();
+    this.state = {
+      card: [],
+      cardName: ""
+    };
+  }
+
+  componentDidMount() {
+    axios.get('https://api.github.com/users/kwmorlock')
+    .then(response => {
+      console.log(response);
+      this.setState({
+        userObject: response.data
+
+      });
+    })
+    .catch(error => {
+      console.log("the data was not returned, error")
+    })
+
+
+  render() {
+    return(
+      <div></div>
+    )
+  }
 }
 
-export default App;
+
+export default App; 
---
+++ usercard/src/Usercard.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+
+class Usercard extends React.component {
+    render() {
+      return(
+        <div></div>
+      )
+    }
+  }
+  
+  
+  export default App; 
